2db7fde9f94aaac5b87982c0abec0825f152ad92
DARSHAN RAJOPADHYE <43828746+therrshan@users.noreply.github.com>
2022-04-23 21:00:16 +0530

    Add files via upload

---
+++ inventory.js
@@ -0,0 +1,25 @@
+function inventory() {
+  return {
+    items: [],
+    add: function(item) {
+        if(this.items.includes(item)) {
+            return
+        }
+        this.items.push(item);
+    },
+    remove: function(item) {
+        if (this.items.includes(item)) {
+            let index = this.items.indexOf(item)
+            if (index > -1) {
+                this.items.splice(index, 1)
+            }
+        }
+    },
+    getList: function() {
+        return this.items
+    }
+  }
+
+}
+
+
